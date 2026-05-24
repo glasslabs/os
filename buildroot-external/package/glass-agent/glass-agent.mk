7fa781df6d06162852179bce0d93b5751a7e4e2e
Nicholas Wiersma <nick@wiersma.co.za>
2026-05-24 16:16:02 +0200

    fix: agent location

--- buildroot-external/package/glass-agent/glass-agent.mk
+++ buildroot-external/package/glass-agent/glass-agent.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 GLASS_AGENT_VERSION = 1.0.0
-GLASS_AGENT_SITE    = $(BR2_EXTERNAL_GLASSOS_PATH)/agent
+GLASS_AGENT_SITE    = $(BR2_EXTERNAL_GLASSOS_PATH)/../agent
 GLASS_AGENT_SITE_METHOD = local
 GLASS_AGENT_LICENSE = MIT
 
